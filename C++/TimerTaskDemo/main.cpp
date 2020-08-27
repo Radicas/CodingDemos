9d6f859fec6e345ea2dd33b8f7a1940137eae171
Rookie <z1176814811@gmail.com>
2020-08-27 15:21:59 +0800

    调用unix的sleep命令，周期执行任务。

--- C++/TimerTaskDemo/main.cpp
+++ C++/TimerTaskDemo/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include "unistd.h"
+#include "TimerTask.h"
+#define TASK_TIMES 5    //执行次数
+#define TASK_PERIOD 2   //执行周期，单位秒
+
+using namespace std;
+
+void TimerTask::setTask(){
+    cout << "任务开始" << endl;
+    int second = TASK_PERIOD*1000000;
+    while (run_index!=TASK_TIMES){
+        cout << run_index << endl;
+        usleep(second);
+        run_index+=1;
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    TimerTask task;
+    task.setTask();
     return 0;
 }
